fd47005765e9b047de6201bae3db16d30c83e20a
fizhes <80635091+fizhes@users.noreply.github.com>
2021-09-03 20:19:51 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -217,6 +217,10 @@ document.getElementById('save').onclick = function() {
   const sky = document.getElementById('sky').value;
   const separators = document.getElementById('separators').value;
   const buttons = document.getElementById('buttons').value;
+  const burger = document.getElementById('burger').checked;
+  const hotdog = document.getElementById('hotdog').checked;
+  const cactus = document.getElementById('cactus').checked;
+  const egg = document.getElementById('egg').checked;
 
 
 
@@ -231,7 +235,7 @@ document.getElementById('save').onclick = function() {
     <H1>Bookmarks</H1>
     <DL><p>
     <DT><H3 ADD_DATE="1543535897" LAST_MODIFIED="1616635351" PERSONAL_TOOLBAR_FOLDER="true">Bookmarks bar</H3>
-    <DT><A HREF="javascript: req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeDarkMode/main/custom_color_scheme.js'); req.onload = function() { eval(this.responseText + \`snake.scheme({ scoreBar: '${score_bar || '#4A752C'}', borders: '${border || '#578A34'}', walls: '${walls || border || '#578A34'}', background: '${(background === '#4A752C' ? score_bar : background) || score_bar || '#4A752C'}', shadows: '${shadows || '#94BD46'}', lightSquares: '${light_squares || '#AAD751'}', darkSquares: '${dark_squares || '#A2D149'}', sky: '${sky || '#4DC1F9'}', separators: '${separators || '#87CEFA'}', buttons: '${buttons || '#1155CC'}', });\`); }; req.send();" ADD_DATE="1618965670">${title}</A>
+    <DT><A HREF="javascript: req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeDarkMode/main/custom_color_scheme.js'); req.onload = function() { eval(this.responseText + \`snake.scheme({ scoreBar: '${score_bar || '#4A752C'}', borders: '${border || '#578A34'}', walls: '${walls || border || '#578A34'}', background: '${(background === '#4A752C' ? score_bar : background) || score_bar || '#4A752C'}', shadows: '${shadows || '#94BD46'}', lightSquares: '${light_squares || '#AAD751'}', darkSquares: '${dark_squares || '#A2D149'}', sky: '${sky || '#4DC1F9'}', separators: '${separators || '#87CEFA'}', buttons: '${buttons || '#1155CC'}', burger: ${burger}, hotdog: ${hotdog}, cactus: ${cactus}, egg: ${egg}, });\`); }; req.send();" ADD_DATE="1618965670">${title}</A>
     </DL><p>    `
   );
 };
